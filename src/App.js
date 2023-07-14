00c76e19f56210267f60dbf49552e84aa55c1575
mridul-wedsin <139317823+mridul-wedsin@users.noreply.github.com>
2023-07-14 03:13:51 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          React App is running.
+          React App is running.Test.
         </p>
       </header>
     </div>
